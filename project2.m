b87b63aef649211550e50deea6f36839d13bd666
Jacob Wilson <wilsonjj2@vcu.edu>
2017-10-07 17:17:05 -0400

    Added Hard Perception training - Need to debug
    Dosen't work yet. Keep getting 0 as all my weights and delta is consistently 0 or 0.06

--- project2.m
+++ project2.m
@@ -1,5 +1,6 @@
 data = csvread('data.csv');
 [train,test] = distributeData(data,0.75);
+[weight,error1] = hardTraining(train,0.3);
 
 function [training,testing] = distributeData(data1,proportion)
 %This function returns two randomly distributed data sets as training and 
@@ -11,6 +12,47 @@ function [training,testing] = distributeData(data1,proportion)
     testing = data1(seq(split+1:end),1:end);
 end
 
-
-
-
+function [weights,error] = hardTraining(trainingData,learningConst)
+%This function trains a perceptron on the training data (<=1000 
+%iterations) and returns the weights of the resulting perceptron
+    np = size(trainingData,1);
+    ite = 1000;
+    alpha = learningConst;
+    epsilon = 0.00001;
+    i = 1;
+    error = 1; %default value
+    weights = sum(rand(3)*10); %randomized starting weights
+    while (i <= ite) && (error > epsilon)
+        for j = 1:np
+            output = trainingData(j,1)*weights(1,1)+trainingData(j,2)*weights(1,2)+weights(1,3);
+            if output > 0
+                output = 1;
+            else
+                output = -1;
+            end
+            if trainingData(j,3) == 1
+                desired = 1;
+            else
+                desired = -1;
+            end
+            delta = alpha*(desired-output);
+            weights = weights*delta;
+        end
+        outputTotal = trainingData(:,1)*weights(1,1)+trainingData(:,2)*weights(1,2)+weights(1,3);
+        desiredTotal = zeros(np,1);
+        for j = 1:size(outputTotal,1)
+           if outputTotal(j,1) > 0
+               outputTotal(j,1) = 1;
+           else
+               outputTotal(j,1) = -1;
+           end
+           if trainingData(j,3) == 1
+               desiredTotal(j,1) = 1;
+           else
+               desiredTotal(j,1) = -1;
+           end
+        end
+        error = sum((desiredTotal(:,1)-outputTotal(:,1)).^2);
+        i = i+1;
+    end
+end
