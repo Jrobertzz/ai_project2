d5e4b878b674c55ea7d3cce940fb56d91e9c7caa
Jrobertzz <robertsjw2@vcu.edu>
2017-10-08 09:30:04 -0400

    altered code to support soft activation, moved alpha to top of code for easily changing alpha in all situations and tweaked alpha slightly

--- project2.m
+++ project2.m
@@ -1,6 +1,9 @@
 data = csvread('data.csv');
+
+Alpha = .002;
+
 [train,test] = distributeData(data,0.75);
-[weight,error1] = hardTraining(train,0.03);
+[weight,error1] = hardTraining(train,Alpha);
 error2 = hardTesting(test,weight);
 
 figure
@@ -15,7 +18,7 @@ xlabel('Height (in)')
 ylabel('Weight (lbs)')
 
 [train,test] = distributeData(data,0.50);
-[weight,error1] = hardTraining(train,0.03);
+[weight,error1] = hardTraining(train,Alpha);
 error2 = hardTesting(test,weight);
 
 figure
@@ -30,7 +33,7 @@ xlabel('Height (in)')
 ylabel('Weight (lbs)')
 
 [train,test] = distributeData(data,0.25);
-[weight,error1] = hardTraining(train,0.03);
+[weight,error1] = hardTraining(train,Alpha);
 error2 = hardTesting(test,weight);
 
 figure
@@ -40,6 +43,51 @@ x = 50 : 1 : 85 ;
 y = -1*(weight(1,3)+weight(1,1)*x)/weight(1,2);
 plot(x,y)
 title1 = strcat('Hard Perceptron with 25% training: Error=',num2str(error2(1,1)));
+title(title1)
+xlabel('Height (in)')
+ylabel('Weight (lbs)')
+
+[train,test] = distributeData(data,0.75);
+[weight,error1] = softTraining(train,Alpha);
+error2 = softTesting(test,weight);
+
+figure
+plot (data(1:2000,1),data(1:2000,2),'+b',data(2001:4000,1),data(2001:4000,2),'+r')
+hold on;
+x = 50 : 1 : 85 ;
+y = -1*(weight(1,3)+weight(1,1)*x)/weight(1,2);
+plot(x,y)
+title1 = strcat('Soft Perceptron with 75% training: Error=',num2str(error2(1,1)));
+title(title1)
+xlabel('Height (in)')
+ylabel('Weight (lbs)')
+
+[train,test] = distributeData(data,0.50);
+[weight,error1] = softTraining(train,Alpha);
+error2 = softTesting(test,weight);
+
+figure
+plot (data(1:2000,1),data(1:2000,2),'+b',data(2001:4000,1),data(2001:4000,2),'+r')
+hold on;
+x = 50 : 1 : 85 ;
+y = -1*(weight(1,3)+weight(1,1)*x)/weight(1,2);
+plot(x,y)
+title1 = strcat('Soft Perceptron with 50% training: Error=',num2str(error2(1,1)));
+title(title1)
+xlabel('Height (in)')
+ylabel('Weight (lbs)')
+
+[train,test] = distributeData(data,0.25);
+[weight,error1] = softTraining(train,Alpha);
+error2 = softTesting(test,weight);
+
+figure
+plot (data(1:2000,1),data(1:2000,2),'+b',data(2001:4000,1),data(2001:4000,2),'+r')
+hold on;
+x = 50 : 1 : 85 ;
+y = -1*(weight(1,3)+weight(1,1)*x)/weight(1,2);
+plot(x,y)
+title1 = strcat('Soft Perceptron with 25% training: Error=',num2str(error2(1,1)));
 title(title1)
 xlabel('Height (in)')
 ylabel('Weight (lbs)')
@@ -98,6 +146,70 @@ function [weights,error] = hardTraining(trainingData,learningConst)
 end
 
 function error = hardTesting(testingData,perceptron)
+    np = size(testingData,1);
+    weights = perceptron;
+    desired = zeros(np,1);
+    for i = 1:np
+        if testingData(i,3) == 1
+            desired(i,1) = 1;
+        else
+            desired(i,1) = -1;
+        end
+    end
+    outputTotal = testingData(:,1)*weights(1,1)+testingData(:,2)*weights(1,2)+weights(1,3);
+    for i = 1:np
+       if outputTotal(i,1) > 0
+           outputTotal(i,1) = 1;
+       else
+           outputTotal(i,1) = -1;
+       end
+    end
+    error = sum((desired(:,1)-outputTotal(:,1)).^2);
+end
+
+function [weights,error] = softTraining(trainingData,learningConst)
+%This function trains a perceptron on the training data (<=1000 
+%iterations) and returns the weights of the resulting perceptron
+    np = size(trainingData,1);
+    ite = 1000;
+    alpha = learningConst;
+    epsilon = 0.00001;
+    i = 1;
+    error = 1; %default value
+    weights = sum(rand(3)); %randomized starting weights
+    while (i <= ite) && (error > epsilon)
+        desired = zeros(np,1);
+        for j = 1:np
+            
+            X = trainingData(j,1)*weights(1,1)+trainingData(j,2)*weights(1,2)+weights(1,3);
+            
+            output = tanh(X);
+            
+            if trainingData(j,3) == 1
+                desired(j,1) = 1;
+            else
+                desired(j,1) = -1;
+            end
+            delta = alpha*(desired(j,1)-output);
+            deltaW = trainingData(j,:);
+            deltaW(1,3) = 1;
+            deltaW = deltaW*delta;
+            weights = weights+deltaW;
+        end
+        outputTotal = trainingData(:,1)*weights(1,1)+trainingData(:,2)*weights(1,2)+weights(1,3);
+        for j = 1:size(outputTotal,1)
+           if outputTotal(j,1) > 0
+               outputTotal(j,1) = 1;
+           else
+               outputTotal(j,1) = -1;
+           end
+        end
+        error = sum((desired(:,1)-outputTotal(:,1)).^2);
+        i = i+1;
+    end
+end
+
+function error = softTesting(testingData,perceptron)
     np = size(testingData,1);
     weights = perceptron;
     desired = zeros(np,1);
